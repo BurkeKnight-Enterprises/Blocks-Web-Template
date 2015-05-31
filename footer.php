752aa4c13a81087e45fb80202a8aa90b3ef507df
Burke Knight <github@burkeknight.com>
2015-05-30 21:36:08 -0400

    Updated footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <div id="footer"><div class="blockheader">Copyrights</div>
 <div class="blockbody"><p>Enter your site copyright here.<br />
 <!-- THE TEMPLATE COPYRIGHT BELOW, NEEDS TO REMAIN. REMOVAL OF IT, IS AGAINST THE LICENSE OF THE TEMPLATE. -->
-Blocks Template by: <a href="http://www.burkeknight.com/" target="_blank">BurkeKnight Enterprises</a>
+Blocks Template by: <a href="http://www.burkeknight.org/" target="_blank">BK-Templates</a>
 <!-- THE TEMPLATE COPYRIGHT ABOVE, NEEDS TO REMAIN. REMOVAL OF IT, IS AGAINST THE LICENSE OF THE TEMPLATE. -->
 </p></div>
 <br /><br /></div></body>
